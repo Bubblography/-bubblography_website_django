1223b1ae9b7f60647c34aa862cb430e17f60a359
Elvinas Jonusas <elvinasjon@gmail.com>
2023-03-20 17:57:32 +0200

    with django(not working)

---
+++ src/static/js/main.js
@@ -0,0 +1,40 @@
+const initApp = () => {
+    const hamburgerBtn = document.getElementById('hamburger-button')
+    const mobileMenu = document.getElementById('mobile-menu')
+
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden')
+        mobileMenu.classList.toggle('flex')
+        hamburgerBtn.classList.toggle('toggle-btn')
+    }
+
+    hamburgerBtn.addEventListener('click', toggleMenu)
+    mobileMenu.addEventListener('click', toggleMenu)
+}
+
+document.addEventListener('DOMContentLoaded', initApp)
+
+  const form = document.querySelector('#contact-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+
+  fetch('/send-email.php', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => {
+    if (response.ok) {
+      alert('Your message has been sent!');
+      form.reset();
+    } else {
+      throw new Error('An error occurred while sending your message.');
+    }
+  })
+  .catch(error => {
+    console.error(error);
+    alert('An error occurred while sending your message. Please try again later.');
+  });
+ });
